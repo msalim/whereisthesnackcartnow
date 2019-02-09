5150cbc148b6bc9b8c7a3301b240e4a485dd1c85
Matthew Salim <matthew.n.salim@gmail.com>
2019-02-09 01:07:52 -0500

    try

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+class Latlong(models.Model):
+	latitude = models.CharField(max_length=100)
+	longitude = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return self.latitude + " " + self.longitude
--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from .models import Latlong
 
 # Create your views here.
 def index(request):
@@ -17,3 +18,19 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+def query(request):
+	latlong = Latlong.objects.order_by('-id')[0]
+	latitude = latlong["latitude"]
+	longitude = latlong["longitude"]
+	return render(request, "query.html", {"latitude" = latitude, "longitude" = longitude})
+
+def submit(request):
+	latitude = request.GET("lat")
+	longitude = request.GET("lon")
+	latlong = Latlong()
+	latlong.latitude = latitude
+	latlong.longitude = longitude
+	latlong.save()
+
+	return
